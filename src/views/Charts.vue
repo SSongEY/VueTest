3085b0561e19531b5a492f02cb28cff9fb5b67ee
Eunyoung Song <seycj47@@gmail.com>
2020-02-21 17:15:14 +0900

    common component - chart

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,50 +1,59 @@
 <template>
-    <div class="Test">
+    <div class="page-dev-charts">
         <v-container>
-            <v-layout justify-space-around row fill-height mt-12>
-                <v-flex xs12 class="text-center">
+            <v-row class="py-8" align="center" justify="start">
+                <v-col cols="12" class="text-center">
                     <span class="display-1 font-weight-light">Custom Chart Examples</span>
                     <br/>
                     <span class="subtitle-1 font-weight-light deepOrange--text">Custom Chart 예제</span>
-                </v-flex>
-            </v-layout>
-            <v-layout align-center justify-space-around row fill-height mt-4>
-                <v-flex xs6 pa-2>
-                    <c-card>
-                        <v-flex xs12>
-                            <div><c-chart-line /></div>
-                        </v-flex>
+                </v-col>
+
+                <!-- EXAMPLES -1 -->
+                <v-col cols="12" lg="6">
+                    <c-card title="line chart">
+                        <c-chart-line
+                            :labels="lineChartData.labels"
+                            :datasets="lineChartData.datasets"
+                        />
                     </c-card>
-                </v-flex>
-                <v-flex xs6 pa-2>
-                    <c-card>
-                        <v-flex xs12>
-                            <div><c-chart-bar /></div>
-                        </v-flex>
+                </v-col>
+
+                <!-- EXAMPLES -2 -->
+                <v-col cols="12" lg="6">
+                    <c-card title="bar chart">
+                        <c-chart-bar
+                            :labels="barChartData.labels"
+                            :datasets="barChartData.datasets"
+                        />
                     </c-card>
-                </v-flex>
-                <v-flex xs6 pa-2>
-                    <c-card>
-                        <v-flex xs12>
-                            <div><c-chart-doughnut /></div>
-                        </v-flex>
+                </v-col>
+
+                <!-- EXAMPLES -3 -->
+                <v-col cols="12" lg="6">
+                    <c-card title="Doughnut Chart">
+                        <c-chart-doughnut
+                            :labels="doughnutChartData.labels"
+                            :datasets="doughnutChartData.datasets"
+                        />
                     </c-card>
-                </v-flex>
-                <v-flex xs6 pa-2>
-                    <c-card>
-                        <v-flex xs12>
-                            <div><c-chart-pie /></div>
-                        </v-flex>
+                </v-col>
+                <v-col cols="12" lg="6">
+                    <c-card title="pie chart">
+                        <c-chart-pie
+                            :labels="pieChartData.labels"
+                            :datasets="pieChartData.datasets"
+                        />
                     </c-card>
-                </v-flex>
-                <v-flex xs6 pa-2>
-                    <c-card>
-                        <v-flex xs12>
-                            <div><c-chart-horizontal-bar /></div>
-                        </v-flex>
+                </v-col>
+                <v-col cols="12" lg="6">
+                    <c-card title="horizontal bar chart">
+                        <c-chart-horizontal-bar
+                            :labels="horizontalChartData.labels"
+                            :datasets="horizontalChartData.datasets"
+                        />
                     </c-card>
-                </v-flex>
-            </v-layout>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
@@ -52,8 +61,98 @@
 <script>
 export default {
     data: () => ({
+        lineChartData: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                'August', 'September', 'October', 'November', 'December'],
+            datasets: [
+                {
+                    label: 'Data One',
+
+                    fill: false,
+                    backgroundColor: 'orange',
+                    borderColor: 'orange',
+                    borderWidth: 3,
+
+                    data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+                },
+                {
+                    label: 'Data two',
+
+                    fill: false,
+                    backgroundColor: 'green',
+                    borderColor: 'green',
+                    borderWidth: 3,
+
+                    data: [50, 30, 40, 60, 100, 20, 30, 50, 60, 80, 100, 110]
+                }
+            ]
+        },
+        barChartData: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: 'red',
+                    //Data to be represented on y-axis
+                    data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+                }
+            ]
+        },
+        doughnutChartData: {
+            labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+            datasets: [
+                {
+                    backgroundColor: [
+                        'grey',
+                        'orange',
+                        'green',
+                        'blue'
+                    ],
+                    data: [40, 20, 80, 10]
+                }
+            ]
+        },
+        pieChartData: {
+            labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+            datasets: [
+                {
+                    backgroundColor: [
+                        'grey',
+                        'orange',
+                        'green',
+                        'blue'
+                    ],
+                    data: [40, 20, 80, 10]
+                }
+            ]
+        },
+        horizontalChartData: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: '#f87979',
+                    data: ['40', '20', '12', '11', '10', '40', '39', '80', '40', '20', '12', '11']
+                }
+            ]
+        }
     }),
+    mounted() {
+        // setTimeout(()=> {
+        //     // this.barChartData.datasets.data = [50, 60, 40, 60, 100, 20, 30, 40, 50, 70, 90, 100]
+        //     this.barChartData = {
+        //         labels: [...this.barChartData.labels],
+        //         datasets: [{
+        //             label: 'Data One',
+        //             backgroundColor: 'green',
+        //             data: [50, 60, 40, 60, 100, 20, 30, 40, 50, 70, 90, 100]
+        //         }]
+        //     }
+        // }, 3000);
+    },
     methods: {
+    },
+    watch: {
     }
 }
 </script>
